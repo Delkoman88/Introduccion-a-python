10ab68e56883d8861cc8e0cd2a2aa113ea960b85
Edgar Mancilla <delkoman@gmail.com>
2020-03-31 20:18:24 -0700

    Reto completado

--- reto1_programas_ramificados.py
+++ reto1_programas_ramificados.py
@@ -4,9 +4,9 @@ edad1 = int(input('Ingresa la edad de la primera persona: '))
 persona2 = input('Ingresa el nombre de la segunda persona: ')
 edad2 = int(input('Ingresa la edad de la segunda persona: '))
 
-if persona1 > persona2:
-    print(f'[persona1] + "es mayor que" + [persona2])') 
-elif persona2 > persona1:
-    print(f'[persona2] + "es mayor que" + [persona1])')
-elif persona1 == persona2:
-    print(f'[persona1] + "y" + [persona2] + "son de la misma edad")')
+if edad1 > edad2:
+    print(f'{persona1} es mayor que {persona2}') 
+elif edad2 > edad1:
+    print(f'{persona2} es mayor que {persona1}')
+elif edad1 == edad2:
+    print(f'{persona1} y {persona2} son de la misma edad')
